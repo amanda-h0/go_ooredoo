45599f2f0dbdef43ee5dad6c460824b08954b707
Amanda Haddad <amandaahdd@outlook.com>
2026-07-09 10:19:36 +0300

    Client-side connection integrated

--- system_monitor/main.go
+++ system_monitor/main.go
@@ -3,11 +3,19 @@ package main
 import (
 	"fmt"
 	"log"
+	"net" // for TCP connection
 
 	"system-monitor/system_functions"
 )
 
 func main() {
+	// Connect to TCP server
+	conn, err := net.Dial("tcp", "localhost:8080")
+	if err != nil {
+		log.Fatalf("Failed to connect to TCP server: %v", err)
+	}
+	defer conn.Close()
+
 	fmt.Println("\nSystem Information:")
 	fmt.Println("-------------------")
 
@@ -31,4 +39,13 @@ func main() {
 		log.Fatalf("Failed to get disk usage: %v", err)
 	}
 	fmt.Printf("Disk Usage: %.2f%%\n\n", DiskUsage)
+
+	// format payload
+	payload := fmt.Sprintf("CPU Usage: %.2f%%\nMemory Usage: %.2f%%\nDisk Usage: %.2f%%\n", CPUUsage, MemoryUsage, DiskUsage)
+	
+	// send data to server
+	_, err = conn.Write([]byte(payload))
+	if err != nil {
+		log.Fatalf("Failed to send data to server: %v", err)
+	}
 }
